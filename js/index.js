5cbfef06e55498f46e7260dcd8d0b23b02bd0677
Santiago utrero <santiago.utrero12@gmail.com>
2023-11-17 23:09:43 -0300

    nueva funcion

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 let costoCantidad = 0
 let costoKey = 0
-let costoEntrega = 0
+let costoPais = 0
 let cantidad = ""
 let key = ""
-let entrega = ""
-
-
+let impuesto = ""
 
 function saludar(){
     alert("hola, vamos a calcular tu proxima compra!")
@@ -18,91 +16,92 @@ function productos(){
 
     switch (cantidad){
         case "1":
-            costoCantidad = 200
+            costoCantidad = 1 * costoKey
             break;
         case "2":
-            costoCantidad = 400
+            costoCantidad = 2 * costoKey
             break;
         case "3":
-            costoCantidad = 600
+            costoCantidad = 3 * costoKey
             break;
         case "4":
-            costoCantidad = 800
+            costoCantidad = 4 * costoKey
             break;
     }
 
     if (costoCantidad <=0)
         alert ("Cantidad no valida")
     else
-        alert (`La cantidad es ${cantidad} a ${costoCantidad} pesos`)
+        alert (`La cantidad es ${cantidad} a ${costoCantidad} usd`)
 
     console.log(cantidad)
 }
 
 function Juego(){
 
-    key = prompt ("Por favor escoja el juego que desee; Hollow knight, Vampire survivors, Soulstone, Minecraft")
+    key = prompt ("Por favor escoja el monto que quiere; Giftcard 5, Giftcard 10, Giftcard 15, Giftcard 20")
 
     costoKey = 0
 
     switch (key){
 
-        case "Vampire survivors":
-            costoKey = 100
+        case "Giftcard 5":
+            costoKey = 5
             break;
-        case "Soulstone":
-            costoKey = 200
+        case "Giftcard 10":
+            costoKey = 10
             break;
-        case "Hollow knight":
-            costoKey = 250
+        case "Giftcard 15":
+            costoKey = 15
             break;
-        case "Minecraft":
-            costoKey = 350
+        case "Giftcard 20":
+            costoKey = 20
             break;
     }
 
     if(costoKey <=0)
-        alert("Juego no valido")
+        alert("no valido")
     else
-        alert(`El juego es ${key} a ${costoKey} pesos`)
+        alert(`La giftcard es de ${costoKey} usd `)
 
     console.log(key)
 }
 
-function Envio(){
+function Impuesto(){
+    impuesto = prompt("Por favor ingrese su pais: Argentina, Brasl, Peru, Colombia")
 
-    entrega = prompt ("Ahora ingrese su plataforma a la cual enviaremos la key: Xbox, Steam, Playstation, Switch")
+    costoPais = 0
 
-    costoEntrega = 0
+    switch(impuesto){
 
-    switch (entrega){
-
-        case "Xbox":
-            costoEntrega = 500
+        case "Argentina":
+            costoPais = 920
             break;
-        case "Steam":
-            costoEntrega = 600
+        case "Brasil":
+            costoPais = 4,86
             break;
-        case "Playstation":
-            costoEntrega = 650
+        case "Peru":
+            costoPais = 3,98
             break;
-        case "Switch":
-            costoEntrega = 800
+        case "Colombia":
+            costoPais = 4026
             break;
     }
 
-    if(costoEntrega <=0)
-        alert("Plataforma no valida")
+    if(costoPais <=0)
+        alert("no valido")
     else
-        alert(`Se enviara a ${entrega} por ${costoEntrega} pesos`)
+        alert(`La conversion es de ${costoKey} usd `)
 
-    console.log(entrega)
-}
+    console.log(key)
+} 
+
 
 function resultado(){
-    alert(" Cantidad de producto: " + costoCantidad + " Juego: " + costoKey + " Envio: " + costoEntrega);
-    suma = costoCantidad + costoKey+ costoEntrega
-    alert ("El total del pedido es de: " + suma);
+    alert(" Cantidad: " + cantidad + " Producto: " + key + " Conversion de 1 dolar: " + costoPais);
+    result = costoCantidad * costoPais
+    alert ("El total del pedido es de: " + result);
+    gmail = prompt("Por favor ingrese su gmail para enviarle la giftcard")
 }
 
 
@@ -110,8 +109,35 @@ function ejecutarPrueba(){
 saludar();
 Juego();
 productos();
-Envio();
+Impuesto();
 resultado();
 }
 
-ejecutarPrueba();
+// ejecutarPrueba();
+
+
+//segunda prueba
+
+const carrito =  []
+const products = [{id: 1, nombre: `gift 10 usd`, precio: 10},
+                {id: 2, nombre: `gift 20 usd`, precio: 20},  
+                {id: 3, nombre: `gift 30 usd`, precio: 30},
+                {id: 4, nombre: `gift 40 usd`, precio: 40},
+                {id: 5, nombre: `gift 50 usd`, precio: 50},
+                {id: 6, nombre: `gift 60 usd`, precio: 60},
+                {id: 7, nombre: `gift 70 usd`, precio: 70},
+                {id: 8, nombre: `gift 80 usd`, precio: 80},
+                {id: 9, nombre: `gift 90 usd`, precio: 90},
+                {id: 10, nombre: `gift 100 usd`, precio:100}]
+
+// se tiene que indicar en los parentesis la id a buscar
+
+function buscar(id){
+    let productoSeleccionado = products.find((products)=> products.id === id )
+
+    if(id <=0)
+        alert("no valido")
+    else
+        alert(`Tu id es ${id}`)
+    console.table(productoSeleccionado)
+}

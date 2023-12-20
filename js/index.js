fed14027265c8487bbce5a43fe4d78c2ea84dcc6
Santiago utrero <santiago.utrero12@gmail.com>
2023-12-19 22:15:00 -0300

    carrito

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// slider
+
 let currentSlide = 0;
         const slides = document.querySelectorAll('.slider-img');
         const totalSlides = slides.length;
@@ -27,3 +29,76 @@ let currentSlide = 0;
         }
 
         setInterval(nextSlide, 3000);
+
+//carrito
+
+const contenidoTienda = document.getElementById("contenidoTienda");
+const verCarrito = document.getElementById("verCarrito");
+const modalContenido = document.getElementById("modalContenido");
+
+let carrito = [];
+
+Productos.forEach((producto)=> {
+    let contenido = document.createElement("div");
+    contenido.className = "cards"
+    contenido.innerHTML = `
+    <img src="${producto.img}" alt="${producto.nombre}">
+    <h2>${producto.nombre}</h2>
+    <p>${producto.precio} $</p>
+    `
+
+    contenidoTienda.append(contenido);
+
+    let comprar = document.createElement("button");
+    comprar.innerText = "comprar";
+    comprar.className = "button-comprar"
+
+    comprar.addEventListener("click", () => {
+        carrito.push({
+            id : producto.id,
+            img : producto.img,
+            nombre : producto.nombre,
+            precio : producto.precio
+        });
+        console.log(carrito)
+    })
+    contenido.append(comprar);
+});
+
+verCarrito.addEventListener("click", () => {
+    modalContenido.innerHTML = "";
+    modalContenido.style.display = "flex"
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML = `
+    <h3 class="modal-header-titulo">Checkout</h3>
+    `;
+    modalContenido.append(modalHeader);
+
+    const modalButton = document.createElement("a");
+    modalButton.innerText = "❌";
+    modalButton.className = "button-modal";
+
+    modalButton.addEventListener("click", () => {
+        modalContenido.style.display = "none";
+    });
+
+    modalHeader.append(modalButton);
+
+    carrito.forEach((producto) => {
+    let carritoContenido = document.createElement("div");
+    carritoContenido.className = "carrito-contenido"
+    carritoContenido.innerHTML = `
+    <img src="${producto.img}" alt="${producto.nombre}">
+    <h3>${producto.nombre}</h3>
+    <p>${producto.precio} $</p>
+    `;
+    modalContenido.append(carritoContenido);
+    })
+
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+    const totalCompra = document.createElement("div")
+    totalCompra.className = "total-contenido"
+    totalCompra.innerHTML = `Total a pagar: ${total} $`
+    modalContenido.append(totalCompra);
+})
---
+++ js/products.js
@@ -0,0 +1,9 @@
+const Productos = [
+    {id: 1, nombre: "gift steam 10", precio: 10, img: "../images/10usdsteam.webp"},
+    {id: 2, nombre: "gift steam 20", precio: 20, img: "../images/20usdsteam.webp"},
+    {id: 3, nombre: "gift steam 50", precio: 50, img: "../images/50usdsteam.webp"},
+    {id: 4, nombre: "gift steam 100", precio: 100, img: "./images/100usdsteam.webp"},
+    {id: 5, nombre: "Nintendo 3 months", precio: 30, img: "./images/nintendo3Month.jpg"},
+    {id: 6, nombre: "Nintendo 12 months", precio: 60, img: "./images/nintendo12Month.jpg"},
+    
+];
